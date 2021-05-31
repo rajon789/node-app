0cba24fd6777a133d11c138c18fa722784db858c
Rajon <rajon@example.com>
2021-05-31 10:39:43 -0400

    added first

---
+++ index.js
@@ -0,0 +1,8 @@
+const express = require("express");
+const app = express();
+
+app.get("/",(req, res) => {
+    res.send("<h2>Welcome to Express Server!!!</h2>")
+});
+const port = process.env.PORT || 3000;
+app.listen(port, () => console.log(`listening on port ${port}`));
